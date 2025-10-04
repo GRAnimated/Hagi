8627756a280fb6e4dc7951828b94072e19ff3aa8
GRAnimated <devin@nhscan.com>
2025-10-04 13:49:45 -0400

    Reglob when new source files are added

--- tools/build.py
+++ tools/build.py
@@ -4,6 +4,16 @@ import argparse
 from setup import get_build_dir
 import subprocess
 import os
+import time
+from common import setup_common as setup
+
+project_root = setup.ROOT
+
+def touch_cmake_lists():
+    cmake_lists_path = project_root / 'CMakeLists.txt'
+    os.utime(cmake_lists_path, times=None)
+
+build_sources_path = get_build_dir() / '.build_sources'
 
 def main():
     parser = argparse.ArgumentParser(
@@ -15,7 +25,7 @@ def main():
     args = parser.parse_args()
 
     if not get_build_dir().is_dir():
-        print("Please run setup.py first.")
+        print("Please run tools/setup.py first.")
         exit(1)
 
     cmake_args = ['cmake', '--build', str(get_build_dir())]
@@ -24,10 +34,28 @@ def main():
     if args.verbose:
         os.environ['VERBOSE'] = '1'
 
-    try:
-        subprocess.run(cmake_args, check=True)
-    except subprocess.CalledProcessError:
-        exit(1)  # silently exit with failure if build failed
+    # Touch CMakeLists.txt if necessary, for reglobbing
+
+    # Adding real source files
+    real_source_paths = []
+    for dir in [project_root/'src']:
+        for root, _, files in os.walk(dir):
+            for file in files:
+                file_path = os.path.join(root, file)
+                file_str = str(file_path)
+                if file_str.endswith('.h') or file_str.endswith('.hpp') or file_str.endswith('.c') or file_str.endswith('.cpp'):
+                    real_source_paths.append(str(file_path))
+
+    # Checking if real files/files from build folder match
+    with open(build_sources_path, 'r') as f:
+        paths = f.read().splitlines()
+        paths.sort()
+        real_source_paths.sort()
+
+        if paths != real_source_paths:
+            touch_cmake_lists()
+
+    subprocess.run(cmake_args)
 
 if __name__ == "__main__":
     main()
